bdd12cea501a28a8853a7da208189ab83bc19b54
Ricardo Roman <ricardomon85@gmail.com>
2015-12-06 21:51:13 -0600

    Initial Commit

---
+++ AccesoWebTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AccesoWebTable
+//
+//  Created by Ricardo Roman Landeros on 06/12/15.
+//  Copyright © 2015 lagunahack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
