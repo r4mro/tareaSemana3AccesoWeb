cbb3cd9c4c65ce84de9859b63cf1dc5dbcbbd6cf
Ricardo Roman <ricardomon85@gmail.com>
2015-12-06 23:35:14 -0600

    app para subir

---
+++ AccesoWebTable/TableViewController.swift
@@ -0,0 +1,116 @@
+//
+//  TableViewController.swift
+//  AccesoWebTable
+//
+//  Created by Ricardo Roman Landeros on 06/12/15.
+//  Copyright © 2015 lagunahack. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TableViewController: UITableViewController {
+    
+    var books = [NSManagedObject]()
+    
+    override func viewWillAppear(animated: Bool) {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        
+        
+        //2
+        let fetchRequest = NSFetchRequest(entityName: "Book")
+        
+        do {
+            let results = try managedContext.executeFetchRequest(fetchRequest)
+            books = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return books.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+
+        // Configure the cell...
+        let book = books[indexPath.row]
+        cell.textLabel!.text = book.valueForKey("name") as? String
+
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- AccesoWebTable/ViewController.swift
+++ AccesoWebTable/ViewController.swift
@@ -7,8 +7,30 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+//    @IBOutlet var textISB: UITextField!
+//    
+//    @IBOutlet var textResultado: UITextView!
+//    
+//    @IBOutlet weak var lbNombreLibro: UILabel!
+//    
+//    @IBOutlet weak var ivImagenLibro: UIImageView!
+    
+    
+    @IBOutlet weak var textISB: UITextField!
+    
+    @IBOutlet weak var textResultado: UITextView!
+    
+    @IBOutlet weak var lbNombreLibro: UILabel!
+    
+    @IBOutlet weak var ivImagenLibro: UIImageView!
+    
+    var name: String = ""
+    var autor: String = ""
+    var imageText: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +41,96 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func saveBook(name: String, autor: String, image: String)
+    {
+        let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+        let managedContext = appDelegate!.managedObjectContext
+        let entity = NSEntityDescription.entityForName("Book", inManagedObjectContext: managedContext)
+        
+        let book = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        //3
+        book.setValue(name, forKey: "name")
+        book.setValue(autor, forKey: "autor")
+        book.setValue(image, forKey: "image")
+        
+        //4
+        do {
+            try managedContext.save()
+            //5
+            print("datos guardados biennnnnnnnnn")
+            //  people.append(person)
+            
+        } catch let error as NSError {
+            
+            print("No se salvo  \(error), \(error.userInfo)")
+            
+        }
+    }
 
+    @IBAction func buttonBuscar(sender: UIButton) {
+            let text1 = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
+            let urls = text1 + textISB.text!
+            //let urls = "http://dia.ccm.itesm.mx/"
+            let isbn: String = "ISBN:" + textISB.text!
+            //print(urls)
+            let url = NSURL(string: urls)
+            let datos: NSData? = NSData(contentsOfURL: url!)
+            if datos == nil
+            {
+                print("errrorr")
+            }
+                else
+            {
+                do{
+                let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                let diccionario1 = json as! NSDictionary
+                let diccionario2 = diccionario1[isbn] as! NSDictionary
+                name = diccionario2["title"] as! NSString as String
+                lbNombreLibro.text = name
+                let arrayAutor = diccionario2["authors"] as! NSArray
+                let dicAtores = arrayAutor[0] as! NSDictionary
+                print("Aqui vienen lso autores")
+                print(dicAtores["name"]!)
+                autor = dicAtores["name"]! as! NSString as String
+                textResultado.text = autor
+                if diccionario2.valueForKey("cover") == nil
+                {
+                    print("No contiene imagen")
+                    let image = UIImage(named: "no.png")
+                    self.ivImagenLibro.image = image
+                    imageText = "no tiene imagen"
+                    print("\(name), \(autor), \(imageText)")
+                   saveBook(name, autor: autor, image: imageText)
+                }
+                    else
+                {
+                    let dicImagen = diccionario2["cover"] as! NSDictionary
+                    let urlImgeText = dicImagen["large"] as! NSString as String
+                    imageText = urlImgeText
+                    let urlImage = NSURL(string: urlImgeText)
+                    let datosImagen: NSData? = NSData(contentsOfURL: urlImage!)
+                    let image = UIImage(data: datosImagen!)
+                    self.ivImagenLibro.image = image!
+                    print("\(name), \(autor), \(imageText)")
+                    saveBook(name, autor: autor, image: imageText)
+                }
+                
+                //print("\(name), \(autor), \(imageText)")
+                //saveBook(name, autor: autor, image: imageText)
+                
+                
+            }
+                catch _{
+                        
+                }
+                //let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+                //textResultado.text = String(texto)
+                //print(texto!)
+            }
+            
+            }
 
 }
 
